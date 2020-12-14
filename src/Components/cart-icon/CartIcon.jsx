0eb7f064fb722283f9bb9df2f8de0b7d83457add
stellawobble <jennidemuir@gmail.com>
2020-12-13 18:28:47 -0800

    added addItem and removeItem functionality to checkout item component

--- src/Components/cart-icon/CartIcon.jsx
+++ src/Components/cart-icon/CartIcon.jsx
@@ -8,10 +8,10 @@ import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
 
     </div>
 
