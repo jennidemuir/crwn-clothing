6b8906b96687ed04d2266279c7e9843af26194ec
stellawobble <jennidemuir@gmail.com>
2021-01-14 18:19:35 -0800

    on Shop page, clicking on item title will redirect to item page

--- src/Components/collection-preview/CollectionPreview.jsx
+++ src/Components/collection-preview/CollectionPreview.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { withRouter } from "react-router-dom";
 
 import CollectionItem from '../collection-item/CollectionItem';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, linkUrl, items, history, match }) => (
+  
   <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
+    <h1 className='title'
+    onClick={() => history.push(`${match.url}/${title.toLowerCase()}`)}
+    >{title.toUpperCase()}</h1>
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
@@ -17,4 +21,4 @@ const CollectionPreview = ({ title, items }) => (
   </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+
 import CollectionsOverview from '../../Components/collections-overview/CollectionsOverview';
-
-
 import CollectionPage from '../collection/CollectionPage';
+
+
 
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    
   </div>
 );
 
